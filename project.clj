2ccf80dbb25c3675d75650bb80ac53802634b4a3
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-04-05 17:37:07 +0200

    [feature] find invalid names on sharepoint, extract features from name

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject fds2app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojars.smee.common "1.2.0"]])
