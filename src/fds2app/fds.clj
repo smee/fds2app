d990c1e4c3a4c159e640c7e4105fb6f688da1963
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2012-04-20 12:52:54 +0200

    [feature] annotate/inject/enhance FDS-Nodes: combine data from different sources into one common tree

--- src/fds2app/fds.clj
+++ src/fds2app/fds.clj
@@ -1,22 +1,12 @@
 (ns fds2app.fds
   (:refer-clojure :exclude [type]))
 
-(defprotocol fds-node
+(defprotocol Fds-Node
   "This protocol knows how to handle external datasources, locates data within and links it to other datasources."
   (children [this] "Collection of fds-nodes.")
   (properties [this])
   (type [this])
   (id [this]))
-
-
-(deftype SimpleNode [id type properties children]
-  fds-node
-  (children   [_] children)
-  (type       [_] type)
-  (properties [_] properties)
-  (id         [_] id))
-
-
 
 (defn tree-of [fds-node]
   (tree-seq (constantly true) children fds-node))
@@ -27,9 +17,35 @@
 (defn find-by-id [key fds-node]
   (first (find-by #(= key (id %)) fds-node)))
 
+;(defprotocol Node-Enhancer
+  ;"Query for additional informations about a fds-node"
+  ;(related [this ^fds-node node]))
+(defrecord Enhanced-Node [node node-enhancers]
+  Fds-Node
+  (id [_] (id node))
+  (properties [_] (properties node))
+  (type [_] (type node))
+  (children [_]
+            (->> node-enhancers
+              (map #(% node))
+              (apply concat (children node))
+              (map #(->Enhanced-Node % node-enhancers)))))
+
+(defn enhanced-tree 
+  "Any function in node-enhancers may inspect an instance of Fds-Node and return child nodes
+from any source."
+  [fds-node & node-enhancers]
+  (Enhanced-Node. fds-node node-enhancers))
+
 
 
 (comment 
+  (defrecord SimpleNode [id type properties children]
+    Fds-Node
+    (children   [_] children)
+    (type       [_] type)
+    (properties [_] properties)
+    (id         [_] id))
   
   (def example 
     (SimpleNode. "a" "number" {:value 1}
@@ -40,4 +56,12 @@
   (find-by-id "a21" example) ;; => #fds2app.fds.SimpleNode{:id "a21", :value 100, :type "number", :children []}
   (map id (find-by #(-> % properties :value even?) example));; => ("a1" "a21")
   (map id (find-by #(contains? (properties %) :foo) example)) ;; => ("a21")
+  ;; inject nodes into example tree, also add to injected node dynamically
+  (def foo (enhanced-tree example 
+                          #(when (= "a" (id %)) 
+                             [(SimpleNode. "injected" "foobar" {:value 55, :misc "baz"} [])])
+                          #(when (= "foobar" (type %)) 
+                             [(SimpleNode. "injected-injected" "another type" {:some :properties} [])])))
+  (map (comp count children) (tree-of foo))
   )
+
